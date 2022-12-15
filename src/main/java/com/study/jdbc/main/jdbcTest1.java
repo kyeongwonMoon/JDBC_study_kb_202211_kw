b3b58a2214d5f8ce3b32815a7fd6777b2450514b
문경원 <mkw1999@naver.com>
2022-12-15 10:31:58 +0900

    jdbc1

---
+++ src/main/java/com/study/jdbc/main/jdbcTest1.java
@@ -0,0 +1,39 @@
+package src.main.java.com.study.jdbc.main;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import main.java.com.study.jdbc.util.DBConnection;
+
+public class jdbcTest1 {
+
+	public static void main(String[] args) {
+		
+		Connection connection = DBConnection.getInstance().getConnection();
+		
+		System.out.println(connection);
+
+		String sql = "select * from score_mst";
+		
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			
+			System.out.println("id\tname\t\tscore");
+			
+			while(rs.next()) {
+				System.out.println("id: " + rs.getInt(1) 
+				+ "\t name: " + rs.getString(2) 
+				+ "\t score: " + rs.getInt(3));
+			
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
